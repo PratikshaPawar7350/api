f4dc3d3fbcef474351b1425e86052fbe8c34a52a
PratikshaPawar7350 <107267479+PratikshaPawar7350@users.noreply.github.com>
2024-01-19 22:17:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express=require("express");
-const cors=require("cors");
-const app=express();
+const express = require("express");
+const cors = require("cors");
 const port=process.env.PORT ||3000;
+const app = express();
 app.use(cors());
-const apidata=require("./data.json");
-app.get("/",(req,res)=>{
-    res.send("hello i am live");
 
+const apidata = require("./data.json");
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello, I am live" });
 });
-app.get("/service",(req,res)=>{
-    res.send(apidata);
-}
-)
 app.listen(port,()=>{
     console.log(" Again hello i am live");
 
+})
+app.get("/service", (req, res) => {
+  res.json(apidata);
 });
+
+// Use 'exports' instead of 'module.exports' when not using the 'functions' folder
+exports.handler = app;

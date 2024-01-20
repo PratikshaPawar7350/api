2331a823d41470bd19d6b8092592a4c588173115
PratikshaPawar7350 <107267479+PratikshaPawar7350@users.noreply.github.com>
2024-01-20 15:36:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const port=process.env.PORT ||3000;
 const app = express();
 app.use(cors());
 
@@ -9,13 +8,10 @@ const apidata = require("./data.json");
 app.get("/", (req, res) => {
   res.json({ message: "Hello, I am live" });
 });
-app.listen(port,()=>{
-    console.log(" Again hello i am live");
 
-})
 app.get("/service", (req, res) => {
   res.json(apidata);
 });
 
-// Use 'exports' instead of 'module.exports' when not using the 'functions' folder
-exports.handler = app;
+// Export the app for Netlify Functions
+module.exports = app;
